b09b5e3df9795743218c21f6ebadbd34e009f46d
AlexGradus <countr@mail.ru>
2023-12-11 12:59:32 +0300

    add model User

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'app/auth/auth.module';
+import { PrismaService } from 'libs/prisma/prisma.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
