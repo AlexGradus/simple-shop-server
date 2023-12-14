a6fd2b6e1772b59c48e35e324c7a902aa838cf88
AlexGradus <countr@mail.ru>
2023-12-14 13:28:28 +0300

    feat:add User

---
+++ src/domain/user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsString, IsOptional } from 'class-validator';
+
+export class UserDto {
+  @IsEmail()
+  email: string;
+ 
+  @IsOptional()
+  @IsString()
+  password?: string;
+ 
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+}
